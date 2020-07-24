653f280f41b7c0d9979bc2ba4103046059984ed1
crazy 18 <1099461301@qq.com>
2020-07-24 21:58:57 +0800

    7.24

---
+++ printtest1/printTest1.py
@@ -0,0 +1,14 @@
+# import datetime
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# def printtest1():
+#     print(datetime.datetime.now())
+# scheduler = BlockingScheduler()
+# scheduler.add_job(printtest1,'interval',seconds=5, id='printtest1')
+# scheduler.start()
+from datetime import *
+from apscheduler.schedulers.blocking import BlockingScheduler
+scheduler = BlockingScheduler()
+def printtest1(text):
+    print(text)
+scheduler.add_job(printtest1,'date',run_date = datetime(2020,7,24,21,57,00), args=['测试'])
+scheduler.start()
